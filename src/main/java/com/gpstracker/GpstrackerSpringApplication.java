f49592b2930f89af11e5d309d6d5ec4c8f636d7c
adrian.siewiera <adrian.siewiera@aptitudesoftware.com>
2022-12-17 18:01:15 +0100

    Initial commit

---
+++ src/main/java/com/gpstracker/GpstrackerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.gpstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpstrackerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpstrackerSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gpstracker/HelloControllerTest.java
@@ -0,0 +1,29 @@
+package com.gpstracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldReturnHelloMessage() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/hello"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("Siema Siwy!"));
+    }
+
+}
